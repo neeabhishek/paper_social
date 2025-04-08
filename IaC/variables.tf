45523ebb813763f5c3e9413ff9631db56fff691e
Abhishek Neelakandan <64977046+neeabhishek@users.noreply.github.com>
2025-04-08 12:58:25 +0530

    Add files via upload

---
+++ IaC/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+    type = string
+    default = "ap-south-1"
+}
+variable "ami" {
+    type = string
+    default = "ami-0e35ddab05955cf57"
+}
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
